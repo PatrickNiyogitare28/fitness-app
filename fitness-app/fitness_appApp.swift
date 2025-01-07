1597860d7da595e88ce5423df94bf6ca50af5528
Patrick Niyo <patrickniyogitare28@gmail.com>
2025-01-07 18:49:38 +0200

    Initial Commit

---
+++ fitness-app/fitness_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  fitness_appApp.swift
+//  fitness-app
+//
+//  Created by Patrick Niyo on 07/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct fitness_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

ce575cf37dfc245d62c3dc5cc81f2b4240c4838c
Patrick Niyo <patrickniyogitare28@gmail.com>
2025-01-11 03:20:14 +0200

    Refactor HomeView and add WorkoutCard component
    - Replaced ContentView with FitnessTabView in the app entry point.
    - Enhanced HomeView to include a new state variable for mock workouts and integrated WorkoutCard for displaying workout details.
    - Updated layout to use NavigationStack and LazyVGrid for improved navigation and organization of fitness activities and recent workouts.
    - Added WorkoutCard component to encapsulate workout information, including title, duration, date, and calories burned.

---
+++ fitness-app/Home/WorkoutCard.swift
@@ -0,0 +1,58 @@
+//
+//  WorkoutCard.swift
+//  fitness-app
+//
+//  Created by Patrick Niyo on 11/01/2025.
+//
+
+import SwiftUI
+
+struct Workout {
+    let id: Int
+    let title: String
+    let image: String
+    let duration: String
+    let date: String
+    let calories: String
+    let tinColor: Color
+}
+struct WorkoutCard: View {
+    @State var workout: Workout
+    
+    var body: some View {
+        HStack{
+            Image(systemName: workout.image)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 48, height: 48)
+                .foregroundColor(workout.tinColor)
+                .padding()
+                .background(.gray.opacity(0.1))
+                .cornerRadius(10)
+            
+            VStack(spacing: 16){
+                HStack{
+                    Text(workout.title)
+                        .font(.title3)
+                        .bold()
+                    
+                    Spacer()
+                    
+                    Text(workout.duration)
+                }
+                
+                HStack{
+                    Text(workout.date)
+                    
+                    Spacer()
+                    
+                    Text(workout.calories)
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    WorkoutCard(workout: Workout(id: 1, title: "Running", image: "figure.run", duration: "25 mins", date: "Aug 4", calories: "423 kcal", tinColor: .cyan))
+}
